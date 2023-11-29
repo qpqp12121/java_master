b9017b43196448c9f80015b5b265a3d59321fa89
admin <admin@DESKTOP-ER81RF8>
2023-11-29 17:48:14 +0900

    23.11.29 최종수업

---
+++ hgjava/src/todo/p20231129/MemberService.java
@@ -0,0 +1,18 @@
+package todo.p20231129;
+
+public class MemberService {
+	
+//	private Mem[] members;
+	
+//	public MemberService() {
+//		members = new Member[] { new Member("hong", "12345")
+//	};
+//	
+//	
+//	boolean logIn(String id, String pw) {
+//		for(int i = 0; i < members.length; i++) {
+//			if(members[i] != null && members[i])
+//		}
+//	}
+	
+}
---
+++ hgjava/src/todo/p20231129/MemberServiceExample.java
@@ -0,0 +1,15 @@
+package todo.p20231129;
+
+public class MemberServiceExample {
+	
+	public static void main(String[] args) {
+		//268p 문제 3번을 구현하세요
+		
+		MemberService memberService = new MemberService();
+		
+		
+		
+		
+		
+	}
+}
