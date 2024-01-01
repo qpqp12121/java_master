8699e13c431bc7a3f1e18d2e7983e1a42655ff26
user <user@DESKTOP-BJ01AGC>
2024-01-02 00:52:41 +0900

    01.02(1)

--- BoardWeb/src/main/webapp/javascript/js/dom1.js
+++ BoardWeb/src/main/webapp/javascript/js/dom1.js
@@ -1,18 +1,43 @@
 //dom1.js
- // [ {id, name,phone}... ] ---밖에는 []:배열 {}:object
-     //데이터
-     const obj1 = {id: 'user1', name: '홍길동', phone: '010-1111'} // key: value 반복해서 들어가는 게 object
-     const obj2 = {id: 'user2', name: '김민수', phone: '010-2222'}
-     const obj3 = {id: 'user3', name: '박철호', phone: '010-3333'}
 
-     //배열에 데이터 담기
-     const friends = [obj1, obj2, obj3];
+/* [ 배열 {객체 key: value } ]
+   [ {id, name,phone}... ]     */
 
-     //속성key의 개수만큼 반복할 땐 for ... in.. 추천
-      for(let friend of friends) {
-        for(let prop in friend){
-            console.log(prop.toUpperCase(), '-', friend[prop]);
-            //prop:속성출력 - obj[prop] 값 가져오기  
-            //속성하나의 값 읽어올 때 방법2가지: obj1['id'] or obj1.id / obj1['name'] or obj1.name...
-          }
+  //객체생성
+  const obj1 = {
+    id: 'user1', 
+    name: '홍길동', 
+    phone: '010-1111'
+  };
+
+  const obj2 = {
+    id: 'user2',
+    name: '김민수',
+    phone: '010-2222'
+  };
+  
+  const obj3 = {
+    id: 'user3', 
+    name: '박철호', 
+    phone: '010-3333'
+  };
+
+  //객체들 배열로 저장
+  const friends = [obj1, obj2, obj3];
+
+  //for..of : 배열의 요소 인덱스 번호 없이 바로 활용O
+  //for..in : 객체 key & value 가져옴 (객체순회)
+  for(let friend of friends) {
+    for(let prop in friend){
+        console.log(prop.toUpperCase(), '-', friend[prop]); // ID - user1 
       }
+  }
+
+  //삭제하기
+  // for(let friend of friends){
+  //   console.log(friend, friend["id"]); 
+  // }
+  
+
+  //+ prop:속성출력 - obj[prop] 값 가져오기  
+  //속성하나의 값 읽어올 때 방법2가지: obj1['id'] or obj1.id / obj1['name'] or obj1.name...
